2a3828ad503efbcc4268a0ea44633fc1624ac5f2
aomay <yma1605@163.com>
2016-05-04 01:00:32 +0800

    用户名密码的验证

--- php/login.php
+++ php/login.php
@@ -1,1 +1,20 @@
-<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+<?php 
+	$pdo=new PDO('mysql:dbname=book','root');
+	$username=$_POST["username"];
+	$password=$_POST["password"];
+	$st=$pdo->query("SELECT COUNT(*) FROM users WHERE name='".$username."' AND password='".$password."'");
+	if($st->fetchColumn())
+	{
+		?>
+		<script>alert("登录成功");window.location.href="../views/index.html";</script>
+		<?php
+	}
+
+?>
+</head>
+</html>
